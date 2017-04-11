cc9c5b1f06254e31df79ae5df9204acc46193bd5
贝竹欣 <wb-beizhuxin.qs@alibaba-inc.com>
2017-04-11 23:23:06 +0800

    初步添加buttonGroup

--- src/components/eaglesoft/buttonGroup.vue
+++ src/components/eaglesoft/buttonGroup.vue
@@ -6,6 +6,20 @@
 <script>
   export default {
     name: 'EgButtonGroup',
+    props: {
+      // 按钮组的尺寸
+      size: {
+        type: String,
+        default: ''
+      },
+      // 按钮组的形状
+      shape: {
+        type: String,
+        default: ''
+      },
+      // 按钮组是否纵向排列
+      vertical: Boolean
+    },
     data () {
       return {
       }
